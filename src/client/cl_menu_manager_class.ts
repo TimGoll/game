954930723d4310cb8bb10a230e9711c9525db517
Tim Goll <tim@timgoll.de>
2019-03-24 21:26:45 +0100

    added documentation to code

--- src/client/cl_menu_manager_class.ts
+++ src/client/cl_menu_manager_class.ts
@@ -21,24 +21,58 @@ export interface IMenu {
 export class MenuManager {
     menues : IMenuDictionary = {};
     element : HTMLElement;
+    displayed: boolean;
 
     constructor(element_id : string) {
         this.element = document.getElementById(element_id);
     }
 
-    register_menu(menu_page : IMenu) {
+    /**
+     * registers a new menu that can be displayed
+     * @param menu_page A menu interface with all its elements
+     * @returns returns false when there's already a menu with this ID
+     */
+    register_menu(menu_page : IMenu) : boolean {
+        if (this.menues[menu_page.id] != undefined)
+            return false;
+
         this.menues[menu_page.id] = menu_page;
+        return true;
     }
 
-    display_menu(id: string) {
+    /**
+     * Displays a menu once this function in called. Moreover this function enables the menu resizing
+     * @param id An alphanumerical ID that defines the menu that should be rendered. Unknown IDs are ignored.
+     * @returns if if was successful
+     */
+    display_menu(id: string) : boolean {
         let menu = this.menues[id];
         if (menu == undefined)
-            return;
+            return false;
+
+        this.displayed = true;
 
         this.element.innerHTML = menu.title; // clear menu
         for (let i = 0; i < menu.elements.length; i++) {
             this.element.appendChild(this.generate_element(menu.elements[i]));
         }
+
+        return true;
+    }
+
+    /**
+     * Removes the current menu and therefore disables menu rescaling
+     */
+    hide_menu() : void {
+        this.element.innerHTML = '';
+        this.displayed = false;
+    }
+
+    recalc_menu() : void {
+        if (!this.displayed)
+            return;
+
+        // ToDo recalc size and position values
     }
 
     generate_element(element : IMenuElement) : HTMLElement { //TODO: this is a placeholder menu generator
